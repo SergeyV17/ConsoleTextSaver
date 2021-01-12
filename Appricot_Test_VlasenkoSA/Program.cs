392a8e416e7dea002614c5ecad2fb10f14fac132
Vlasenko Sergey <Lust209@mail.ru>
2021-01-12 23:03:56 +1000

    Инициализация репозитория.

---
+++ Appricot_Test_VlasenkoSA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Appricot_Test_VlasenkoSA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter text line and press Enter for new line. Press Ctrl + S to Save file");
+
+            ConsoleKeyInfo cki;
+            Console.TreatControlCAsInput = true;
+
+            do
+            {
+                cki = Console.ReadKey();
+
+                //if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+                //if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+                //if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+
+                if (cki.Key == ConsoleKey.Enter)
+                    Console.WriteLine();
+
+
+            } while (!(cki.Modifiers == ConsoleModifiers.Control &&  cki.Key == ConsoleKey.S));
+
+            Console.WriteLine("File saved");
+        }
+    }
+}
