efc3865f33f29c0efd19ef712e04a7b902388378
Vlasenko Sergey <Lust209@mail.ru>
2021-01-13 00:45:28 +1000

    Добавлена логика сохранения файла с текстом на жёсткий диск.

--- Appricot_Test_VlasenkoSA/Program.cs
+++ Appricot_Test_VlasenkoSA/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.IO;
 
 namespace Appricot_Test_VlasenkoSA
 {
@@ -11,21 +13,31 @@ namespace Appricot_Test_VlasenkoSA
             ConsoleKeyInfo cki;
             Console.TreatControlCAsInput = true;
 
+            StringBuilder sb = new StringBuilder();
+
             do
             {
                 cki = Console.ReadKey(true);
 
                 if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("");
                 else if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("");
-                else Console.Write(cki.KeyChar);
+                else
+                {
+                    Console.Write(cki.KeyChar);
+                    sb.Append(cki.KeyChar);
+                }
 
                 if (cki.Key == ConsoleKey.Enter)
                     Console.WriteLine();
 
-
             } while (!(cki.Modifiers == ConsoleModifiers.Control &&  cki.Key == ConsoleKey.S));
 
-            Console.WriteLine("\nFile saved");
+
+            string path = string.Format($"{DateTime.Now:dd-MM-yyyy-HH-mm-ss}.txt");
+
+            File.WriteAllText(path, sb.ToString());
+
+            Console.WriteLine($"\nFile successfully saved. {new FileInfo(path).Length} bytes");
         }
     }
 }
