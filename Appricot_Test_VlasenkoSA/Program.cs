e7182d3b4f1bb66fe2dad51598fb5f27a7456e3f
Vlasenko Sergey <Lust209@mail.ru>
2021-01-13 12:06:28 +1000

    Добавлена логика обработки клавиши backspace. Логика обработчика текста вынесна в отдельный класс

--- Appricot_Test_VlasenkoSA/Program.cs
+++ Appricot_Test_VlasenkoSA/Program.cs
@@ -4,38 +4,62 @@ using System.IO;
 
 namespace Appricot_Test_VlasenkoSA
 {
-    class Program
+    static class TextReader
     {
-        static void Main()
+        internal static string Read()
         {
-            Console.WriteLine("Enter text line and press Enter for new line. Press Ctrl + S to Save file");
-
-            ConsoleKeyInfo cki;
             Console.TreatControlCAsInput = true;
 
             StringBuilder sb = new StringBuilder();
+            ConsoleKeyInfo cki;
 
             do
             {
                 cki = Console.ReadKey(true);
 
-                if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("");
-                else if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("");
-                else
-                {
-                    Console.Write(cki.KeyChar);
-                    sb.Append(cki.KeyChar);
-                }
+                if (cki.Modifiers == ConsoleModifiers.Control && cki.Key == ConsoleKey.S)
+                    return sb.ToString();
 
                 if (cki.Key == ConsoleKey.Enter)
+                {
+                    sb.Append(cki.KeyChar);
                     Console.WriteLine();
+                }
+                else if (cki.Key == ConsoleKey.Backspace)
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb = sb.Remove(sb.Length - 1, 1);
+                        Console.Write(cki.KeyChar);
+                        Console.Write(' ');
+                        Console.Write(cki.KeyChar);
+                    }
+                }
+                else if ((cki.Modifiers & ConsoleModifiers.Alt) != 0)
+                    Console.Write("");
+                else if ((cki.Modifiers & ConsoleModifiers.Control) != 0)
+                    Console.Write("");
+                else
+                {
+                    sb.Append(cki.KeyChar);
+                    Console.Write(cki.KeyChar);
+                }
+            }
+            while (true);
+        }
+    }
 
-            } while (!(cki.Modifiers == ConsoleModifiers.Control &&  cki.Key == ConsoleKey.S));
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text line and press Enter for new line. Press Ctrl + S to Save file");
 
+            string text = TextReader.Read();
 
             string path = string.Format($"{DateTime.Now:dd-MM-yyyy-HH-mm-ss}.txt");
 
-            File.WriteAllText(path, sb.ToString());
+            File.WriteAllText(path, text);
 
             Console.WriteLine($"\nFile successfully saved. {new FileInfo(path).Length} bytes");
         }
